ecee740a2cce0272a805a2a14640f200ad55aefe
sukhee-jung <sukhee.jung@kurlycorp.com>
2024-12-02 14:16:16 +0900

    Update kurly.py

--- kurly.py
+++ kurly.py
@@ -1,11 +1,9 @@
 class KurlyCluster:
-    def __init__(self, channel, nx, ny, name):
+    def __init__(self, channel, name):
         self.channel = channel
-        self.nx = nx
-        self.ny = ny
         self.name = name
 
 # 컬리 클러스터 슬랙 채널 목록
 clusters = [
-    KurlyCluster('C05NXMZP62Y', '56', '127', '김포 클러스터'),
+    KurlyCluster('C083K7HCS73', '공지사항_평택'),
 ]
